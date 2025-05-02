278e5ef372536cbaaff88055556554afdf55f614
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-02 14:28:02 +0100

    Added redux dev tool and persist so it has a local storage

--- src/store.ts
+++ src/store.ts
@@ -1,41 +1,46 @@
-import { create } from "zustand"
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 type Task = {
-  title: string
-  state: "PLANNED" | "ONGOING" | "DONE"
-}
+  title: string;
+  state: "PLANNED" | "ONGOING" | "DONE";
+};
 
 type StoreProps = {
   tasks: Task[];
-  addTask: (title: string, state: Task["state"]) => void
-  deleteTask: (title: string) => void
-  draggedTask: string | null
-  setDraggedTask: (title: string | null) => void
+  draggedTask: string | null;
+  addTask: (title: string, state: Task["state"]) => void;
+  deleteTask: (title: string) => void;
+  setDraggedTask: (title: string | null) => void;
   moveTask: (title: string, state: Task["state"]) => void;
-}
+};
 
-// Task["state"] is a union of string literal and it's another way of writing "PLANNED" | "ONGOING" | "DONE"
-
-export const useStore = create<StoreProps>((set) => ({
-  tasks: [{ title: "TestTask", state: "PLANNED" }],
-  draggedTask: null,
-  addTask: (title, state) =>
-    set((store) => ({
-      tasks: [...store.tasks, { title, state }]
-    })),
-
-    deleteTask: (title: string) =>
-    set((store) => ({
-      tasks: store.tasks.filter((task)=>task.title !== title)
-    })),
-    
-    setDraggedTask: (title: string  | null) => set({draggedTask: title}),
-    moveTask: (title: string, state: Task["state"]) =>
-    set((store) => ({
-    tasks: store.tasks.map((task) =>
-      task.title === title ? { ...task, state } : task
-    ),
-  })),
-}));
-
-// set lets you update the store, when you call set with a function, Zustand gives you the current state of the store — that’s what store is.
+export const useStore = create<StoreProps>()(
+  devtools(
+    persist(
+      (set) => ({
+        tasks: [],
+        draggedTask: null,
+        addTask: (title, state) =>
+          set((store) => ({
+            tasks: [...store.tasks, { title, state }],
+          })),
+        deleteTask: (title) =>
+          set((store) => ({
+            tasks: store.tasks.filter((task) => task.title !== title),
+          })),
+        setDraggedTask: (title) => set({ draggedTask: title }),
+        moveTask: (title, state) =>
+          set((store) => ({
+            tasks: store.tasks.map((task) =>
+              task.title === title ? { ...task, state } : task
+            ),
+          })),
+      }),
+      {
+        name: "task-storage", // used by persist in localStorage
+      }
+    )
+    // devtools has no name config here — it uses the store's name by default or shows "anonymous"
+  )
+);
