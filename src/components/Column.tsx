d370d5693f24ae30f3f87a889e2446d90a9bd6d6
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-02 08:01:46 +0100

    The add button is fully functional and there is an input elemwnt to add task

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -8,23 +8,39 @@ type ColumnProps = {
 };
 
 const Column = ({ state }: ColumnProps) => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
   const tasks = useStore((store) => store.tasks);
   const filtered = useMemo(
     () => tasks.filter((task) => task.state === state),
     [tasks, state]
   );
 
-  const addTask = useStore((store) => store.addTask)
+  const addTask = useStore((store) => store.addTask);
 
   return (
     <div className="column">
       <div className="titlewrapper">
         <p>{state}</p>
-        <button onClick={() =>{addTask("dcjdj", state)}}>Add</button>
+        <button onClick={() => setOpen(true)}>Add</button>
       </div>
       {filtered.map((task) => (
         <Task title={task.title} key={task.title} />
       ))}
+      {open && (
+        <div className="modal">
+        <div className="modalContent">
+          <input onChange={(e) => setText(e.target.value)} value={text} />
+          <button
+            onClick={() => {
+              addTask(text, state);
+              setText("");
+              setOpen(false);
+            }}
+          >Add</button>
+        </div>
+      </div>
+      )}
     </div>
   );
 };
