7866c52728747aa46200c81b95848496a603a876
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-02 06:41:36 +0100

    When you change the task the task can now move to it's respective component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,23 +1,25 @@
+import { useStore } from "../store";
 import "./Task.css";
 import classNames from "classnames";
-
-const STATUS = "PLANNED";
 
 type TaskProps = {
   title: string;
 };
 
 const Task = ({ title }: TaskProps) => {
+  const task = useStore((store) =>
+    store.tasks.find((task) => task.title === title)
+  );
   return (
     <div className="task">
-        <div>{title}</div>
-        <div>
-            <div className="bottomrapper">
-              <div className={classNames('status', STATUS)}>{STATUS}</div>
-            </div>
+      <div>{task?.title}</div>
+      <div>
+        <div className="bottomrapper">
+          <div className={classNames("status", task?.state)}>{task?.state}</div>
         </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Task;
